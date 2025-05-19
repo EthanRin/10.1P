e390c6d9a99490302549a4715ef24dbe9cd89648
EthanRin <sovanpanhaarinn@gmail.com>
2025-05-19 21:30:43 +1000

    minor fix

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ const app = express();
 const PORT = 3000;
 const HOST = "0.0.0.0";
 
-// Serve static files from "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// API endpoint for coin toss
 app.get('/toss', (req, res) => {
   const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
   res.json({ result });
 });
 
-// Start server
 app.listen(PORT, HOST, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
